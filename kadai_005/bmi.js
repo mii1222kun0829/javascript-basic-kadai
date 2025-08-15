0e42549ec4cd6173cf6358b56fe2f0f682aee037
mii <sae0829.1222@gmail.com>
2025-08-15 16:36:09 +0900

    MBI出力

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,4 +4,8 @@ let bodyWeight = 68;
 //身長の宣言・値の代入
 let height = 1.7;
 
-console.log(bodyWeight / (height * height));
+//BMI
+let bmi = bodyWeight / (height * height);
+
+//BMIの値を出力
+console.log(bmi);
